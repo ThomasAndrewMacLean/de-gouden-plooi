758de87d87b165e76a308ade41604ba0595d421b
ThomasAndrewMacLean <ThomasAndrewMacLean@users.noreply.github.com>
2021-02-15 08:37:30 +0000

    Deploying to gh-pages from  @ 2f03497e7dd816db755ba7d2ebb295298dbb5d1c 🚀

---
+++ _next/static/kSkXDvSWPaZvvw_hLS1Nq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/atelier/[id]":[a],"/index":[a]}}("static\u002Fchunks\u002Fd478d7c4425e1a46225fab36581edebf3e804489.a1592238159b38de21d5.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
